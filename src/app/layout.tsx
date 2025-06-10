8ee9d37d1860053701af0dbfd8fb28d267b72e3a
Harveen Atwal <harveenatwalbusiness@gmail.com>
2025-06-09 23:04:57 -0700

    add google script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,19 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9S9ZF5SL3W"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9S9ZF5SL3W');
+          `}
+        </Script>
         {/* Twitter conversion tracking base code */}
         <Script id="twitter-pixel" strategy="afterInteractive">
           {`
